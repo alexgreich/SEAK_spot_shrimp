09435d456b4d488152f9bed53d986f3420e1aecc
Alex <alex.reich@alaska.gov>
2023-07-19 15:41:26 -0800

    I have data that is not cut off now

--- Exploration/Code/Earnest_sound_length_exploration.R
+++ Exploration/Code/Earnest_sound_length_exploration.R
@@ -3,8 +3,7 @@
 ### the preliminary explorations for creating a lenght-based model for areas where we have fishery independent data
 
 
-#read in data
-Data <- read.csv("./Exploration/Data/Detailed_Pot_Data.csv")
+#Data has been deleted, does not matter
 
 #get the earnest sound data
 names(Data)
@@ -16,8 +15,10 @@ unique(Data$Area.Description)
 ##107-20 is the area code for upper earnest sound, but check with biologists on this
 ##also there are no length data on that dataset
 ##but there are carapace length data here:
-Specimens_with_potdata <- read.csv("./Exploration/Data/Specimens_with_Pot_Data.csv")
-Specimens_without_potdata <- read.csv("./Exploration/Data/Specimens_without_Pot_Data.csv")
+Specimens_with_potdata_1 <- read.csv("./Exploration/Data/Specimens_with_Pot_Data_1996_2003.csv")
+Specimens_with_potdata_2 <- read.csv("./Exploration/Data/Specimens_with_Pot_Data_2004_2023.csv")
+Specimens_with_potdata <- 
+Specimens_without_potdata <- read.csv("./Exploration/Data/Specimens_without_Pot_Data_2004_2023.csv")
 
 #focus on the with pot data for now.
 library(tidyverse)
@@ -35,3 +36,21 @@ ggplot(Upper_ernest_sound_spec_w_potdata) + aes(x=Carapace.Length.Millimeters, f
 
 #hmm we only have three years?
 unique(Upper_ernest_sound_spec_w_potdata$Year) #yep...
+
+
+###how many years do we hace of the not pot data?
+Upper_ernest_sound_spec_wout_potdata <- Specimens_without_potdata %>% filter(Stat.Area=="107-20")
+unique(Upper_ernest_sound_spec_wout_potdata$Year)
+##we only have three years of data...
+#graphing
+ggplot(Upper_ernest_sound_spec_wout_potdata) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year) +
+  theme_bw() + scale_fill_viridis_d() #this looks the same. Maybe the same dataset with less info
+
+
+#how many years are in this dataset?
+unique(Specimens_with_potdata$Year)
+##hmm. something in the data is messed up?
+View(Upper_ernest_sound_spec_w_potdata)
+
+
+#somethign is wrong with the data. Hidden little angry message at the end of the csv. Something glitched up in the download
