1c674401a5fb57d64f377496309d838e648f9920
Alex <alex.reich@alaska.gov>
2023-07-19 16:06:49 -0800

    Revised until line 57, dont run below that line for now

--- Exploration/Code/Earnest_sound_length_exploration.R
+++ Exploration/Code/Earnest_sound_length_exploration.R
@@ -1,13 +1,14 @@
 ##this script is for playing around with the shrimp dataset
-###specifically with Upper Earnest Sound
+###specifically with Upper Ernest Sound
+####The imported data has be pre-filtered and is only stat area 107-20: upper ernest sound.
 ### the preliminary explorations for creating a lenght-based model for areas where we have fishery independent data
 
 
 #Data has been deleted, does not matter
 
 #get the earnest sound data
-names(Data)
-unique(Data$Area.Description)
+#names(Data)
+#unique(Data$Area.Description)
 ##looking at the area discriptuons, there are these ones of interest
 ##Upper Ernest sound; Upper Ernest Sound; Ernest; Ernest Sound; Ernest SOund; Ervest Sound; Eernest Sound; 
 ##Ernest Sound-Niback/Bold Islands;2 other variations of this spelling;
@@ -17,26 +18,43 @@ unique(Data$Area.Description)
 ##but there are carapace length data here:
 Specimens_with_potdata_1 <- read.csv("./Exploration/Data/Specimens_with_Pot_Data_1996_2003.csv")
 Specimens_with_potdata_2 <- read.csv("./Exploration/Data/Specimens_with_Pot_Data_2004_2023.csv")
-Specimens_with_potdata <- 
-Specimens_without_potdata <- read.csv("./Exploration/Data/Specimens_without_Pot_Data_2004_2023.csv")
+Specimens_without_potdata_1 <- read.csv("./Exploration/Data/Specimens_without_Pot_Data_1996_2006_107_20.csv")
+Specimens_without_potdata_2 <- read.csv("./Exploration/Data/Specimens_without_Pot_Data_2007_2023.csv")
+
+library(tidyverse)
+Specimens_with_potdata<- bind_rows(Specimens_with_potdata_1, Specimens_with_potdata_2)
+Specimens_without_potdata <- bind_rows(Specimens_without_potdata_1, Specimens_without_potdata_2)
 
 #focus on the with pot data for now.
-library(tidyverse)
 names(Specimens_with_potdata)
-unique(Specimens_with_potdata$Stat.Area)
-Upper_ernest_sound_spec_w_potdata <- Specimens_with_potdata %>% filter(Stat.Area=="107-20")
 
 
 #graph the lengths
 library(ggplot2)
 library(viridis)
 
-ggplot(Upper_ernest_sound_spec_w_potdata) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year) +
-  theme_bw() + scale_fill_viridis_d()
+ggplot(Specimens_with_potdata) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year) +
+  theme_bw()#+scale_fill_viridis_d()
 
-#hmm we only have three years?
-unique(Upper_ernest_sound_spec_w_potdata$Year) #yep...
+ggplot(Specimens_with_potdata) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year, ncol=2) +
+  theme_bw()
 
+ggplot(Specimens_with_potdata_1) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year, ncol=1) +
+  theme_bw() #allows us to see an age class, sort of
+
+Specimens_with_potdata_2.1 <- Specimens_with_potdata_2 %>%
+  filter(Year >= 2014)
+
+Specimens_with_potdata_2.2 <- Specimens_with_potdata_2 %>%
+  filter(Year < 2014)
+
+ggplot(Specimens_with_potdata_2.1) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year, ncol=1) +
+  theme_bw()#allows us to see an age class, sort of
+
+ggplot(Specimens_with_potdata_2.2) + aes(x=Carapace.Length.Millimeters, fill=Year, alpha=0.5) + geom_density() + facet_wrap(~Year, ncol=1) +
+  theme_bw()#allows us to see an age class, sort of
+
+####alter below before run
 
 ###how many years do we hace of the not pot data?
 Upper_ernest_sound_spec_wout_potdata <- Specimens_without_potdata %>% filter(Stat.Area=="107-20")
