6acc759bb7c1fc2fe31f491fa7c33bd01941f856
Alex <alex.reich@alaska.gov>
2023-07-25 14:49:31 -0800

    In the process of dataset cleaning

--- Exploration/Code/Earnest_sound_length_exploration.R
+++ Exploration/Code/Earnest_sound_length_exploration.R
@@ -3,27 +3,26 @@
 ####The imported data has be pre-filtered and is only stat area 107-20: upper ernest sound.
 ### the preliminary explorations for creating a lenght-based model for areas where we have fishery independent data
 
+##107-20 is the area code for upper ernest sound
 
-#Data has been deleted, does not matter
-
-#get the earnest sound data
-#names(Data)
-#unique(Data$Area.Description)
-##looking at the area discriptuons, there are these ones of interest
-##Upper Ernest sound; Upper Ernest Sound; Ernest; Ernest Sound; Ernest SOund; Ervest Sound; Eernest Sound; 
-##Ernest Sound-Niback/Bold Islands;2 other variations of this spelling;
-##Earnest Sound
-##107-20 is the area code for upper earnest sound, but check with biologists on this
-##also there are no length data on that dataset
-##but there are carapace length data here:
+#read in data
 Specimens_with_potdata_1 <- read.csv("./Exploration/Data/Specimens_with_Pot_Data_1996_2003.csv")
 Specimens_with_potdata_2 <- read.csv("./Exploration/Data/Specimens_with_Pot_Data_2004_2023.csv")
-Specimens_without_potdata_1 <- read.csv("./Exploration/Data/Specimens_without_Pot_Data_1996_2006_107_20.csv")
-Specimens_without_potdata_2 <- read.csv("./Exploration/Data/Specimens_without_Pot_Data_2007_2023.csv")
 
 library(tidyverse)
 Specimens_with_potdata<- bind_rows(Specimens_with_potdata_1, Specimens_with_potdata_2)
-Specimens_without_potdata <- bind_rows(Specimens_without_potdata_1, Specimens_without_potdata_2)
+
+
+#clean data
+library(lubridate)
+Specimens_with_potdata_int <- Specimens_with_potdata %>%
+  mutate(Soak.Time = Time.Lift.Begin - Time.Set.Begin)
+
+Specimens_with_potdata_c <- Specimens_with_potdata_int %>% 
+  filter(Sample.Type=="Size") %>%
+  filter(Species=="Spot shrimp") %>%
+  filter(Pot.Condition=="Normal")
+  
 
 #focus on the with pot data for now.
 names(Specimens_with_potdata)
